fe4254d952c1fc1a6441cefb342ed13ad7a117aa
Jessica Bohn <jbohn.developer@gmail.com>
2025-07-18 03:21:02 -0500

    Create basic forced game logic

--- lib/game.rb
+++ lib/game.rb
@@ -15,13 +15,59 @@ class Game
   end
 
   def play
-    # Logic for playing the game
+    puts 'This means War! (the game)...'
+    puts "Number of players: #{players.size}"
+
+    puts 'Dealing cards...'
+    deal_cards
+
+    play_round(players) until game_over?
+  end
+
+  def deal_cards
+    dealt_cards = deck.deal(players.size)
+    players.each_with_index do |player, index|
+      player.hand = dealt_cards[index]
+    end
   end
 
   private
 
+  def choose_players(num)
+    allowed_numbers = [2, 4]
+    # If no number is provided, randomly choose between 2 or 4 players
+    num_players = num || allowed_numbers.sample
+    # If there were a UI component to this, the given number would be validated and an error raised if invalid
+    # num_players = allowed_numbers.include?(num) ? num : allowed_numbers.sample
+    num_players.times.map { |i| Player.new("Player #{i + 1}", []) }
+  end
+
   def play_round(active_players, winnings = [])
-    # Logic for playing a round of the game
-    # This would involve comparing cards, determining the winner, etc.
+    plays = active_players.map { |p| [p, p.draw_card] }.to_h
+
+    max_card = plays.values.compact.max
+    winners = plays.select { |_, card| card == max_card }.keys
+    winnings.concat(plays.values)
+    if winners.size > 1
+      war_cards = []
+
+      winners.each do |winner|
+        3.times { war_cards << winner.draw_card } # Face-down cards
+        war_cards << winner.draw_card             # Face-up war card
+      end
+
+      winnings.concat(war_cards.compact)
+      winners.reject!(&:out_of_cards?)
+
+      return if winners.empty?
+
+      play_round(winners, winnings)
+    else
+      winners.first.add_cards(winnings)
+    end
+  end
+
+  def game_over?
+    players.count { |player| !player.out_of_cards? } <= 1
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,4 +15,38 @@ RSpec.describe Game do
     expect(game.players.size).to eq(2)
     expect(game.deck.cards.size).to eq(52)
   end
+
+  it 'starts with each player having an empty hand' do
+    expect(game.players.all? { |player| player.hand.empty? }).to be true
+  end
+
+  context '#choose_players' do
+    it 'chooses the correct number of players' do
+      expect(game.players.size).to eq(num_players)
+    end
+
+    it 'defaults to 2 or 4 players if no argument is given' do
+      game = Game.new
+      expect(game.players.size).to eq(2).or eq(4)
+    end
+  end
+
+  context '#deal_cards' do
+    it 'deals cards to each player' do
+      game.deal_cards
+      expect(game.players.all? { |player| !player.hand.empty? }).to be true
+    end
+
+    it 'deals cards evenly among players' do
+      game.deal_cards
+      expect(game.players.map(&:hand).map(&:size).uniq.size).to eq(1)
+    end
+  end
+
+  context '#play_round' do
+    it 'plays a round of the game' do
+      game.deal_cards
+      expect { game.send(:play_round, game.players) }.to(change { game.players.map(&:hand).map(&:size) })
+    end
+  end
 end
